9f3b1af6e2858541c39fea9ea9c09147fae70383
root <sanjaypatni@yahoo.com>
2024-11-17 13:36:23 +0000

    updates

--- install2.sh
+++ install2.sh
@@ -3,8 +3,9 @@ docker stop `docker ps -q`
 docker container prune -f
 docker image remove sanjaypatni5/api:latest
 docker image remove sanjaypatni5/content:latest
-docker run -d sanjaypatni5/api:latest
-docker run -d sanjaypatni5/content:latest
-docker run -d sanjaypatni5/apigateway:latest
-docker run -d sanjaypatni5/eureka:latest
-docker run -d sanjaypatni5/upload:latest
+docker image remove sanjaypatni5/eureka:latest
+docker image remove sanjaypatni5/apigateway:latest
+docker image remove sanjaypatni5/upload:latest
+docker login
+docker-compose pull
+docker-compose up -d
