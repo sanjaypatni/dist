1e7c46e267ba4332a8f3e2e5714a6932b092df51
Sanjay Patni <sanjaypatni@yahoo.com>
2024-12-03 13:35:58 +0000

    updates

--- kafka.sh
+++ kafka.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-nohup java -jar /root/dist/events/target/event-0.0.1-SNAPSHOT.jar   >> /tmp/output.txt &
+nohup java -jar /root/dist/events/target/event-0.0.1-SNAPSHOT.jar   >> nohup.out 2>> nohup.err < /dev/null  &
 sleep 10
-nohup ./bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties >> /tmp/output.txt
+nohup ./bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties >>  nohup.out 2>> nohup.err < /dev/null  &
 sleep 10
-nohup ./bin/kafka-server-start.sh -daemon ./config/server.properties >> /tmp/output.txt
+nohup ./bin/kafka-server-start.sh -daemon ./config/server.properties >> nohup.out 2>> nohup.err < /dev/null  & 
 sleep 10
-./bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092 >>  /tmp/output.txt  2>> /tmp/err_file
+nohup ./bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092 >>  nohup.out 2>> nohup.err < /dev/null  &
 sleep 10
 exit 0
