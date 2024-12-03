756d1e609c4c1f12f3f03e3c23285a846e8f993d
Sanjay Patni <sanjaypatni@yahoo.com>
2024-12-03 12:22:32 +0000

    redirecting output.txt

--- kafka.sh
+++ kafka.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-nohup java -jar /root/dist/events/target/event-0.0.1-SNAPSHOT.jar &
-sleep 1
-nohup ./bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties
-sleep 1
-nohup ./bin/kafka-server-start.sh -daemon ./config/server.properties
-sleep 1
-./bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092
-sleep 1
+nohup java -jar /root/dist/events/target/event-0.0.1-SNAPSHOT.jar   2>> /tmp/output.txt &
+sleep 10
+nohup ./bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties 2>> /tmp/output.txt
+sleep 10
+nohup ./bin/kafka-server-start.sh -daemon ./config/server.properties 2>> /tmp/output.txt
+sleep 10
+./bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092 2>> /tmp/output.txt
+sleep 10
 exit 0
