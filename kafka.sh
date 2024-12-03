858ccfa3a995e69821c26af8d7538757a658d260
Sanjay Patni <sanjaypatni@yahoo.com>
2024-12-03 12:02:10 +0000

    updates

--- kafka.sh
+++ kafka.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#java -jar /root/dist/events/target/event-0.0.1-SNAPSHOT.jar &
-./bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties
-sleep 15
-./bin/kafka-server-start.sh -daemon ./config/server.properties
-sleep 15
+nohup java -jar /root/dist/events/target/event-0.0.1-SNAPSHOT.jar &
+sleep 1
+nohup ./bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties
+sleep 1
+nohup ./bin/kafka-server-start.sh -daemon ./config/server.properties
+sleep 1
 ./bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092
+sleep 1
+exit 0
