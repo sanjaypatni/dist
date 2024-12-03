c1c0ac862da4181b78b9a9a3e99cc6e0d25439e9
Sanjay Patni <sanjaypatni@yahoo.com>
2024-12-03 12:58:41 +0000

    stderr

--- kafka.sh
+++ kafka.sh
@@ -5,6 +5,6 @@ nohup ./bin/zookeeper-server-start.sh -daemon ./config/zookeeper.properties >> /
 sleep 10
 nohup ./bin/kafka-server-start.sh -daemon ./config/server.properties >> /tmp/output.txt
 sleep 10
-./bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092 >> /tmp/output.txt
+./bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092 2>> /tmp/output.txt
 sleep 10
 exit 0
