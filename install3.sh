73ae0e710d8ee9f05e0f7df7381cf9a0211901a2
Sanjay Patni <sanjaypatni@yahoo.com>
2024-12-03 10:38:58 +0000

    removed kafka cpmmand

--- install3.sh
+++ install3.sh
@@ -12,10 +12,3 @@ docker-compose pull
 docker-compose up -d
 cp -R /root/dist.kafka/kafka_2.13-3.1.0 .
 cp -R /root/dist.kafka/events .
-nohup ./kafka_2.13-3.1.0/bin/zookeeper-server-start.sh -daemon ./kafka_2.13-3.1.0/config/zookeeper.properties
-sleep 15
-nohup ./kafka_2.13-3.1.0/bin/kafka-server-start.sh -daemon ./kafka_2.13-3.1.0/config/server.properties
-sleep 15
-./kafka_2.13-3.1.0/bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092
-sleep 15
-nohup java -jar /root/dist/events/target/event-0.0.1-SNAPSHOT.jar
