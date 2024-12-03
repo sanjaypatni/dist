6831a521903a6407f6a5c9b5d9047bf1256ce0c1
sanjaypatni <47679220+sanjaypatni@users.noreply.github.com>
2024-12-03 02:16:37 -0800

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -10,6 +10,13 @@ docker image remove sanjaypatni5/web3j:latest
 docker login
 docker-compose pull
 docker-compose up -d
+cd /root/dist
 cp -R /root/dist.kafka/kafka_2.13-3.1.0 .
 cp -R /root/dist.kafka/events .
-cd /root/dist.kafka/kafka_2.13-3.1.0; ./kafka.sh
+nohup ./kafka_2.13-3.1.0/bin/zookeeper-server-start.sh -daemon ./kafka_2.13-3.1.0/config/zookeeper.properties
+sleep 15
+nohup ./kafka_2.13-3.1.0/bin/kafka-server-start.sh -daemon ./kafka_2.13-3.1.0/config/server.properties
+sleep 15
+./kafka_2.13-3.1.0/bin/kafka-topics.sh --create --topic playclips --bootstrap-server localhost:9092
+sleep 15
+nohup java -jar /root/dist/events/target/event-0.0.1-SNAPSHOT.jar
