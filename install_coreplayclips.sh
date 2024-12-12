a552054a6ff4d8d0212175e0f4f554939fe854e5
sanjay patni <sanjaypatni@yahoo.com>
2024-12-12 09:34:01 +0000

    setup

--- install_coreplayclips.sh
+++ install_coreplayclips.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-apt update
-(sleep 10; echo Y) | apt install openjdk-11-jre-headless
-(sleep 10; echo Y) | apt install docker
-(sleep 10; echo Y) | apt install docker-compose
-(sleep 10; echo Y) | apt install apache2
-cp /root/dist/apache2.conf /etc/apache2/apache2.conf
-a2enmod proxy && sudo a2enmod proxy_http && sudo service apache2 restart
-(sleep 10; echo Y) | apt install python3-certbot-apache
-(sleep 5; echo sanjaypatni@yahoo.com; sleep 5; echo A; sleep 5; echo Y; sleep 5; echo 2 ) | certbot --apache -d coreplayclips.com
-apt install git-lfs
-cd /root/dist
-git lfs fetch --all
-git lfs pull
 docker stop `docker ps -q`
 docker container prune -f
 docker image remove sanjaypatni5/api:latest
@@ -24,6 +11,4 @@ docker image remove sanjaypatni5/message:latest
 (sleep 5; echo sanjaypatni5; sleep 5; echo Apple6490) | docker login
 docker-compose pull
 cp ./kafka.sh  /root/dist/kafka_2.13-3.1.0
-cd /var/lib/docker/volumes;mv dist_mysql-volume dist_mysql-volume.bak
-cd /var/lib/docker/volumes;tar xvf /root/dist/dist_mysql-volume.tar
 cd /root/dist;docker-compose up -d
