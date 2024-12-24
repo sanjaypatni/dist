9c96707082145e977ebf514ab53482f5fd2d25aa
Sanjay Patni <sanjaypatni@yahoo.com>
2024-12-24 02:10:33 -0800

    updates

--- install_coreplayclips.sh
+++ install_coreplayclips.sh
@@ -2,12 +2,11 @@
 docker stop `docker ps -q`
 docker container prune -f
 docker image remove sanjaypatni5/api:latest
+docker image remove sanjaypatni5/playclips-api:latest
 docker image remove sanjaypatni5/content:latest
 docker image remove sanjaypatni5/eureka:latest
 docker image remove sanjaypatni5/apigateway:latest
 docker image remove sanjaypatni5/upload:latest
-docker image remove sanjaypatni5/web3j:latest
-docker image remove sanjaypatni5/message:latest
 (sleep 5; echo sanjaypatni5; sleep 5; echo Apple6490) | docker login
 docker-compose up -d
 cp ./kafka.sh  /root/dist/kafka_2.13-3.1.0
