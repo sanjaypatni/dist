d81c1a7c1ca76d50291912ceca3a09e4f97b79a7
Sanjay Patni <sanjaypatni@yahoo.com>
2024-12-24 02:09:38 -0800

    updates

--- install_core4ed.sh
+++ install_core4ed.sh
@@ -6,6 +6,6 @@ docker image remove sanjaypatni5/content:latest
 docker image remove sanjaypatni5/eureka:latest
 docker image remove sanjaypatni5/apigateway:latest
 docker image remove sanjaypatni5/upload:latest
-docker image remove sanjaypatni5/web3j:latest
+docker image remove sanjaypatni5/playclips-api:latest
 docker login
 docker-compose up -d
